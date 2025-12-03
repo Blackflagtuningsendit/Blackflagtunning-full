ee49fd43dee8c5f4549c3c41cc429cef3d79c78b
Blackflagtuningsendit <scott@blackflagtuning.com>
2025-12-03 04:02:12 -0500

    Create Page.tsx

---
+++ App/Shop/Page.tsx
@@ -0,0 +1,10 @@
+export default function Shop() {
+  return (
+    <div className="min-h-screen bg-black text-white p-4">
+      <h1 className="text-4xl text-redflag text-center py-8">BLACK FLAG SWAG STORE</h1>
+      {/* Add more product grids here, similar to swag section above */}
+      <p className="text-center">Coming soon: Trump Tee, Vance Drop, Tyrus XL, Don Jr. Edition.</p>
+      <a href="/" className="block text-center mt-8 text-redflag">← Back to Home</a>
+    </div>
+  );
+}
